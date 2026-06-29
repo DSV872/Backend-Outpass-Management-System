54dae3547e9face4eb18e8cd6d7b3148a71ec08b
Dasari Santhi Vardhan <dasarisanthivardhan872@gmail.com>
2026-06-29 11:07:33 +0530

    Add files via upload
    oracle-service-registry uploaded

---
+++ service-registry-main/src/test/java/com/outpass/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outpass.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
