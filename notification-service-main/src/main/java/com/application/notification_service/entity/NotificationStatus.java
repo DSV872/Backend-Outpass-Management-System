912c80267b940610f8ad6c91cb776eb15f72d812
Dasari Santhi Vardhan <dasarisanthivardhan872@gmail.com>
2026-08-21 16:31:04 +0530

    Notification service updated with cloud deployment

--- notification-service-main/src/main/java/com/application/notification_service/entity/NotificationStatus.java
+++
@@ -1,9 +0,0 @@
-package com.application.notification_service.entity;
-
-
-public enum NotificationStatus {
-
-	PENDING,
-	SENT,
-	FAILED
-}
--- notification-service-main/src/main/java/com/application/notification_service/entity/NotificationType.java
+++
@@ -1,7 +0,0 @@
-package com.application.notification_service.entity;
-
-public enum NotificationType {
-	
-	EMAIL,IN_APP,SMS
-
-}
